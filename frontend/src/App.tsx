404a7f15758fa7953a65d0d8ddcd8f356d4b6abe
TomssZv <tomsszvirbulis@gmail.com>
2023-03-22 19:49:29 +0200

    VG-2 Create login and register form style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,21 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar />      
-      <Routes>
-        <Route path ='/' element={<Home />} />
-      </Routes>
+      <Navbar />
+      <div className='content'>
+        <Routes>
+          <Route path ='/' element={<Home />} />
+          <Route path ='/login' element={<LoginForm />} />
+          <Route path ='/register' element={<RegisterForm />} />
+        </Routes>
+      </div>   
     </div>
   )
 }
--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function RegisterForm() {
   return (
-    <>
-    </>
+    <div className='form-container'>
+      <form>
+        <h1 className='form-title'>Register</h1>
+        <div className='double'>
+          <div className='field'>
+            <div className='field-title'>Name</div>
+            <input className='field-input'></input>
+          </div>
+          <div className='field'>
+            <div className='field-title'>Name</div>
+            <input className='field-input'></input>
+          </div>
+        </div>
+      
+        <div className='field'>
+          <div className='field-title'>Email</div>
+          <input className='field-input'></input>
+        </div>
+        <div className='field'>
+          <div className='field-title'>Passwords</div>
+          <input className='field-input'></input>
+        </div>
+        <button>Login</button>
+        <p className='register'>Already have a account? <Link to='/login' >login here</Link></p>
+      </form>
+    </div>
   )
 }
 
